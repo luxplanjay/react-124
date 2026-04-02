3335c9ba5d47db8d54f799888214b5f3d943d25f
Alexander Repeta <alexander.repeta@gmail.com>
2026-04-02 20:37:29 +0300

    Add examples

--- README.md
+++ README.md
@@ -1,14 +1,35 @@
-# Заняття 7 - TanStack Query
+# Заняття 8 - Форми з Formik
 
-- Серверні дані та кешування
-- Інсталяція
-  [TanStack Query](https://tanstack.com/query/latest/docs/framework/react/installation)
-- Налаштування `QueryClient`
-- Налаштування `ReactQueryDevtools`
-- Хук `useQuery` та запити при монтуванні компонента
-  - https://swapi.info/api/people/1
-  - Ключі запиту
-- Залежні запити з `enabled`
-- Пагінація запитів
-  - `placeholderData` та `keepPreviousData`
-  - Бібліотека [React Paginate](https://www.npmjs.com/package/react-paginate)
+- Бібліотека [Formik](https://formik.org/)
+- Контейнер форми `Formik` та `Form`
+  - Пропс `initialValues`
+  - Пропс `onSubmit`
+- Поля форми `Field`
+  - Атрибут `name`
+- Стилізація компонентів `Formik`
+- Типізація `initialValues`
+- Параметри `values` та `actions` у `onSubmit`
+- Типи полів через пропс `as`
+- Доступ до властивостей `Formik` у JSX через Render Prop
+- Валідація з [`Yup`](https://github.com/jquense/yup)
+  - Схема валідаціі
+  - Можливі аргументи функції-валідатора
+  - Пропс `validationSchema`
+- Компонент `ErrorMessage`
+  - Пропс `component`
+
+## Повна форма
+
+```tsx
+export default function OrderForm() {
+  const fieldId = useId();
+
+  return (
+    <form className={css.form}>
+      <button type="submit" className={css.button}>
+        Place order
+      </button>
+    </form>
+  );
+}
+```
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,58 +1,9 @@
-import { useQuery, keepPreviousData } from "@tanstack/react-query";
-import { fetchArticles } from "../services/articleService";
-import ArticleList from "./ArticleList";
-import SearchForm from "./SearchForm";
-import { useState } from "react";
-import ReactPaginateModule from "react-paginate";
-import { type ReactPaginateProps } from "react-paginate";
-import { type ComponentType } from "react";
-import css from "./App.module.css";
-
-type ModuleWithDefault<T> = { default: T };
-
-const ReactPaginate = (
-  ReactPaginateModule as unknown as ModuleWithDefault<
-    ComponentType<ReactPaginateProps>
-  >
-).default;
+import OrderForm from "./OrderForm";
 
 export default function App() {
-  const [topic, setTopic] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const { data, isSuccess } = useQuery({
-    queryKey: ["articles", topic, currentPage],
-    queryFn: () => fetchArticles(topic, currentPage),
-    enabled: topic !== "",
-    placeholderData: keepPreviousData,
-  });
-
-  const handleSearch = (newTopic: string) => {
-    setTopic(newTopic);
-    setCurrentPage(1);
-  };
-
-  const totalPages = data?.nbPages ?? 0;
-
   return (
     <>
-      <SearchForm onSubmit={handleSearch} />
-
-      {data && <ArticleList items={data.hits} />}
-
-      {isSuccess && totalPages > 1 && (
-        <ReactPaginate
-          pageCount={totalPages}
-          pageRangeDisplayed={3}
-          marginPagesDisplayed={3}
-          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
-          forcePage={currentPage - 1}
-          containerClassName={css.pagination}
-          activeClassName={css.active}
-          nextLabel="->"
-          previousLabel="<-"
-        />
-      )}
+      <OrderForm />
     </>
   );
 }
